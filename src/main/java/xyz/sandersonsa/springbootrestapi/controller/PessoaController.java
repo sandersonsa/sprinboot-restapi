e44364fde7106068fc727e3d56816bb501b8fe4b
Red Hat - Sanderson Sa <sanderson.redhat@fornecedores.klabin.com.br>
2022-04-13 12:55:55 -0300

    atualizar infos de documentacao

--- src/main/java/xyz/sandersonsa/springbootrestapi/controller/PessoaController.java
+++ src/main/java/xyz/sandersonsa/springbootrestapi/controller/PessoaController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
 import xyz.sandersonsa.springbootrestapi.model.Pessoal;
 import xyz.sandersonsa.springbootrestapi.service.PessoaService;
 
@@ -25,13 +26,14 @@ public class PessoaController {
     private PessoaService service;
     
     @GetMapping
+    @Operation(summary = "Listar Pessoas", description = "Listar Pessoas")
     public ResponseEntity<List<Pessoal>> findAll() {
         List<Pessoal> lista = service.listarTodos();
 
         if(Objects.nonNull(lista) && !lista.isEmpty()) {
             return ResponseEntity.ok().body(lista);
         } else {
-            return ResponseEntity.notFound().build();
+            return ResponseEntity.noContent().build();
         }
     }
 
